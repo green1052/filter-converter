cb2dd4da23d6972318eb2989322ee0d34e003b8e
green1052 <34261355+green1052@users.noreply.github.com>
2025-10-25 01:48:25 +0900

    1.0.3

---
+++ rollup.config.ts
@@ -0,0 +1,17 @@
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+
+const config = {
+    input: "index.ts",
+    output: {
+        esModule: true,
+        file: "dist/index.js",
+        format: "es",
+        sourcemap: true
+    },
+    // @ts-ignore
+    plugins: [typescript(), nodeResolve({preferBuiltins: true}), commonjs()]
+};
+
+export default config;
